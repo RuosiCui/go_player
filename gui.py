36be518ee66125507a5e39f6c481464b6b49674b
Ruosi Cui <crs0256@gmail.com>
2026-04-23 11:37:54 -0700

    feat: Add Early Stopping Heuristic based on mathematical confidence

--- gui.py
+++ gui.py
@@ -11,7 +11,7 @@ class GoGUI:
         
         self.size = size
         self.engine = GoEngine(size)
-        self.ai = GoAI(time_limit=5)
+        self.ai = GoAI(time_limit=10)
         
         self.cell_size = 50
         self.margin = 30
